115571e7df1a419be84d2b4076abe239bd620ddd
gsdkira <larreakelly@gmail.com>
2022-04-04 21:57:20 -0400

    installed and connected redux through components and files

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'//grow provides simple animation
 import memories from './images/memories.png'
+import useStyles from './styles'
+
 const App = () => {
+    const classes = useStyles()
+
     return(
       <Container maxwidth="lg">
-          <AppBar position="static" color="inherit">
-              <Typography variant="h2" align="center">Memories</Typography>
-              <img src={memories} alt="memories" height="60" />
+          <AppBar className= {classes.appBar} position="static" color="inherit">
+              <Typography className ={classes.heading} variant="h2" align="center">Memories</Typography>
+              <img src={memories} className={classes.image} alt="memories" height="60" />
           </AppBar>
           
           <Grow in>
