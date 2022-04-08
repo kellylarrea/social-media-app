4b2f709e2b83a9ff1eaab179497192cca7643b21
gsdkira <larreakelly@gmail.com>
2022-04-08 18:30:37 -0400

    added current id to button but not working atm issue is saying onclick is not a function

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TextField, Button, Typography, Paper } from '@material-ui/core'
 import FileBase64 from 'react-file-base64'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector  } from 'react-redux'
 
 import useStyles from './styles'
 import { createPost, updatePost } from '../../actions/posts'
@@ -9,9 +9,19 @@ import { createPost, updatePost } from '../../actions/posts'
 //Get The current id on the post that we are on
 
 const Form = ( { currentId, setCurrentId }) => {
+    console.log('current id ', setCurrentId)
     const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' })
+    // if we have a currentid then we want to loop over state.post and make a find method on it that has the same id as our current id then if not then it will = to null
+    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null)
     const classes = useStyles();
     const dispatch = useDispatch()
+
+    
+    useEffect(() => {
+        //if post exis then we are going to set post data
+        if(post) setPostData(post)
+        //dependency array and say when this should be ran
+    }, [post])
 
     const handleSubmit = (e) => {
         e.preventDefault()
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -5,7 +5,9 @@ export default (posts = [], action) =>{
         return action.payload;
      case 'CREATE':
         return [ ...posts, action.payload];
-         
+      case 'UPDATE':
+         // output of map is an array, single post in map if post._id is equal to the action.payload which is the update post then we want to return the action.payload whixh is the updated post otherwise return just the post as it was without any updates
+         return [...posts.map((post) => post._id ===action.payload._id ? action.payload : post)]
  
      default:
          return posts;
