bc3b781850ba8c6a7eb31f382e6fb75ba70a17d7
gsdkira <larreakelly@gmail.com>
2022-04-08 17:11:34 -0400

    sending posts to singular component as props

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React from "react";
 //retrieve data from global redux store
 import { useSelector } from "react-redux";
+import { Grid, CircularProgress} from '@material-ui/core'
 
 import Post from './Post/Post'
 import useStyles from './styles'
@@ -8,17 +9,26 @@ import useStyles from './styles'
 const Posts = () => {
     // initialize as a hook and return state.posts where we export combine reducers-post.js
     const posts = useSelector((state)=> state.posts)
-
     const classes = useStyles()
 
     console.log(posts)
 
     return(
-        <>
-    <h1>Posts</h1>
-    <Post />
-    <Post />
-    </>
+        //if no posts.length then show circularProgess else grid of posts
+       !posts.length ? <CircularProgress /> : (
+           <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+               { 
+               //loop over post and return
+                   posts.map((post)=>(
+                       //key = post._id and item xs for how large for mobile devices
+                       //as props we will be sending post to each post component
+                       <Grid key={post._id} item xs={12} sm={6}>
+                           <Post post={post}/>
+                       </Grid>
+                   ))
+               }
+           </Grid>
+       )
     )
 }
 
