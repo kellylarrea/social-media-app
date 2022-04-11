c5673558c403102fe04705c6fca41e3d1351e4f1
gsdkira <larreakelly@gmail.com>
2022-04-11 13:01:44 -0400

    fixed post component but having issues with message showing on card content

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -10,6 +10,7 @@ import useStyles from './styles'
 // import posts from "../../../reducers/posts";
 
 const Post = ({ post, setCurrentId }) => {
+    console.log('what is post', post)
     const classes = useStyles();
 
     return (
@@ -20,15 +21,19 @@ const Post = ({ post, setCurrentId }) => {
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
             <div className={classes.overlay2}>
-                <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(post._id)}>
+                <Button 
+                style={{ color: 'white' }} 
+                size="small" 
+                onClick={() => setCurrentId(post._id)}>
                     <MoreHoriIcon fontSize="medium" />
                 </Button>
             </div>
             <div className={classes.details}> 
                 <Typography variant="body2" color="textSecondary">{post.tags.map((tag) => `#${tag} `)}</Typography>
             </div>
+            <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
             <CardContent>
-                <Typography className={classes.title} variant="h5" gutterBottom={post.messsage}></Typography>
+            <Typography variant="h3" gutterBottom>{post.messsage}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
             <Button size="small" color="primary" onClick={()=> {}}>
