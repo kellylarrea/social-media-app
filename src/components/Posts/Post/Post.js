56443934d2c17fc221ab0a43e8e603373c13002e
gsdkira <larreakelly@gmail.com>
2022-04-11 13:06:16 -0400

    fixed post.message

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -15,7 +15,7 @@ const Post = ({ post, setCurrentId }) => {
 
     return (
         <Card className={classes.card}>
-            <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
             <div className={classes.overlay}>
                 <Typography variant="h6">{post.creator}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
@@ -33,7 +33,7 @@ const Post = ({ post, setCurrentId }) => {
             </div>
             <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
             <CardContent>
-            <Typography variant="h3" gutterBottom>{post.messsage}</Typography>
+            <Typography variant="h5" gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
             <Button size="small" color="primary" onClick={()=> {}}>
