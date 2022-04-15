c1fd0a04597e30432cd13f7d422157026490670e
gsdkira <larreakelly@gmail.com>
2022-04-15 12:23:57 -0400

    like counter works but state is not changing

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -6,7 +6,7 @@ import MoreHoriIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
 import { useDispatch } from 'react-redux'
 
-import { deletePost } from "../../../actions/posts";
+import { deletePost, likePost } from "../../../actions/posts";
 
 import useStyles from './styles'
 // import { moveMessagePortToContext } from "worker_threads";
@@ -41,7 +41,7 @@ const Post = ({ post, setCurrentId }) => {
             <Typography variant="h6" gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-            <Button size="small" color="primary" onClick={()=> {}}>
+            <Button size="small" color="primary" onClick={()=> dispatch(likePost(post._id))}>
                 <ThumbUpAltIcon fontSize="small" />
                 Like
                 {post.likeCount}
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,19 +1,21 @@
-export default (posts = [], action) =>{
- switch (action.type) {
-    case 'DELETE':
-       //keep all post except for the ones that are equal to action.payload
-       return posts.filter((post) => post._id !== action.payload)
-    case 'UPDATE':
-      // output of map is an array, single post in map if post._id is equal to the action.payload which is the update post then we want to return the action.payload whixh is the updated post otherwise return just the post as it was without any updates
-      return posts.map((post) => post._id ===action.payload._id ? action.payload : post);
-     case 'FETCH_ALL':
-        //our actual posts action.payload from actions
-        return action.payload;
-     case 'CREATE':
-        return [ ...posts, action.payload];
- 
-     default:
+export default (posts = [], action) => {
+   switch (action.type) {
+      case 'DELETE':
+         //keep all post except for the ones that are equal to action.payload
+         return posts.filter((post) => post._id !== action.payload)
+      //put two cases one below each other and they will both do the same thing so since both update and like are doing similar process the just add both cases
+      case 'UPDATE':
+      case 'LIKE':
+         // output of map is an array, single post in map if post._id is equal to the action.payload which is the update post then we want to return the action.payload whixh is the updated post otherwise return just the post as it was without any updates
+         return posts.map((post) => post._id === action.payload._id ? action.payload : post);
+      case 'FETCH_ALL':
+         //our actual posts action.payload from actions
+         return action.payload;
+      case 'CREATE':
+         return [...posts, action.payload];
+
+      default:
          return posts;
- }
+   }
 
 }
