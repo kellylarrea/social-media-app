090c294e001e8c6d17c42790d043085351724039
gsdkira <larreakelly@gmail.com>
2022-04-18 23:53:39 -0400

    added auth folder component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
+import  Auth  from './components/Auth/Auth'
 
 
 const App = () => {
@@ -14,7 +15,7 @@ const App = () => {
             <Navbar />
             <Switch>
                 <Route path="/" exact component={Home}/>
-                {/* <Route path="/auth" exact component={Auth}/> */}
+                <Route path="/auth" exact component={Auth}/>
             </Switch>
             <Home />
         </Container>
---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Auth = () => {
+    const state = null
+  return (
+    <div>Auth</div>
+  )
+}
