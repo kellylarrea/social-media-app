7d0d8aa858b2a8c1c547208539ca2cf883895ca1
gsdkira <larreakelly@gmail.com>
2022-04-18 23:59:23 -0400

    added some auth

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Avatar, Button, Paper, Grid, Typography, Container} from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import useStyles from './styles'
 
 export const Auth = () => {
-    const state = null
+    const classes = useStyles()
   return (
-    <div>Auth</div>
+   <Container component="main" maxWidth="xs">
+<Paper className={classes.paper} elevation={3}>
+    <Avatar className={classes.avatar}>
+      <LockOutlinedIcon/>
+       </Avatar>
+       </Paper>
+     </Container>
   )
 }
